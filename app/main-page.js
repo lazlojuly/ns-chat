ced5e73ff6ff48a29678bc53ad8e78abddc18dae
Laszlo Gyulai <laszlo.gyulai@concretes-MacBook-Pro-2.local>
2015-09-28 14:03:27 +0100

    Updating licence and default files

--- app/main-page.js
+++ app/main-page.js
@@ -2,7 +2,5 @@ var messageModel = require("./MessageModel");
 function pageLoaded(args) {
     var page = args.object;
     page.bindingContext = messageModel.messageModel;
-
-
 }
 exports.pageLoaded = pageLoaded;
